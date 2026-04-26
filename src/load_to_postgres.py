fb44f00656003405f98d0b5c2ef7ab3cd0c844c8
Sidbewendin Yameogo <yameogoa94@gmail.com>
2026-04-27 01:21:24 +0200

    Restore local PostgreSQL configuration

--- src/load_to_postgres.py
+++ src/load_to_postgres.py
@@ -1,23 +1,17 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-username = "retailadmin"
-password = "25Fondy93?"
-host = "retail-postgres-yameogo.postgres.database.azure.com"
-port = "5432"
-database = "postgres"
-
 engine = create_engine(
-    f"postgresql+psycopg2://{username}:{password}@{host}:{port}/{database}"
+    "postgresql+psycopg2://postgres:postgres@localhost:5432/retail_db"
 )
 
 df = pd.read_csv("data/processed/fact_sales.csv")
 
 df.to_sql(
-    "fact_sales_cloud",
+    "fact_sales",
     engine,
     if_exists="replace",
     index=False
 )
 
-print("Data loaded successfully into Azure PostgreSQL!")
+print("Data loaded successfully into PostgreSQL!")
