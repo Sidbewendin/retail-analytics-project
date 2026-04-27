084cfdca65d4bb39834a65bd6ce6f2746ba014e2
Sidbewendin Yameogo <yameogoa94@gmail.com>
2026-04-27 12:49:13 +0200

    PostgreSQL loading script

--- src/load_to_postgres.py
+++ src/load_to_postgres.py
@@ -1,40 +1,11 @@
-import os
-os.environ["PGCLIENTENCODING"] = "utf8"
+from sqlalchemy import create_engine, text
 
-import pandas as pd
-from sqlalchemy import create_engine
-
-# Create connection to PostgreSQL database
 engine = create_engine(
-    "postgresql+psycopg2://postgres:postgres@localhost:5433/retail_db?client_encoding=utf8"
+    "postgresql+psycopg2://postgres:25Fondy93?@localhost:5433/retail_db"
 )
 
-# Load processed datasets
-# -> Fact table (transactions)
-fact_df = pd.read_csv("data/processed/fact_sales.csv")
+with engine.connect() as conn:
+    result = conn.execute(text("SELECT 1"))
+    print(result.fetchone())
 
-# -> Dimension table (customers)
-customer_df = pd.read_csv("data/processed/customers.csv")
-
-
-# Load data into FactSales table
-# -> append data to existing table (do not overwrite schema)
-fact_df.to_sql(
-    "factsales",
-    engine,
-    if_exists="append",
-    index=False
-)
-
-
-# Load data into DimCustomer table
-# -> append data to existing table
-customer_df.to_sql(
-    "dimcustomer",
-    engine,
-    if_exists="append",
-    index=False
-)
-
-
-print("Data successfully loaded into PostgreSQL (FactSales & DimCustomer)")
+print("Connection successful!")
