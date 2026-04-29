71886d299b5b33cafa447b38d4a872e1cbb577d4
Sidbewendin Yameogo <yameogoa94@gmail.com>
2026-04-29 23:02:37 +0200

    Fix PostgreSQL connection for GitHub Actions

--- src/load_to_postgres.py
+++ src/load_to_postgres.py
@@ -1,25 +1,23 @@
+import os
 import pandas as pd
 from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
 
-engine = create_engine(
-    "postgresql+psycopg2://postgres:25Fondy93?@host.docker.internal:5433/retail_db"
+url = URL.create(
+    drivername="postgresql+psycopg2",
+    username=os.getenv("POSTGRES_USER", "postgres"),
+    password=os.getenv("POSTGRES_PASSWORD", "25Fondy93?"),
+    host=os.getenv("POSTGRES_HOST", "localhost"),
+    port=int(os.getenv("POSTGRES_PORT", "5433")),
+    database=os.getenv("POSTGRES_DB", "retail_db"),
 )
+
+engine = create_engine(url)
 
 fact_df = pd.read_csv("data/processed/fact_sales.csv")
 customer_df = pd.read_csv("data/processed/customers.csv")
 
-fact_df.to_sql(
-    "factsales",
-    engine,
-    if_exists="replace",
-    index=False
-)
-
-customer_df.to_sql(
-    "dimcustomer",
-    engine,
-    if_exists="replace",
-    index=False
-)
+fact_df.to_sql("factsales", engine, if_exists="replace", index=False)
+customer_df.to_sql("dimcustomer", engine, if_exists="replace", index=False)
 
 print("Data successfully loaded into PostgreSQL!")
